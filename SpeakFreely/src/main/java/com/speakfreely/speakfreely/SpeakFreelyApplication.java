efda36bf3e686c278287c35fcbb94aea1dbe94fc
Matthew7064 <matthew.programowanie1@gmail.com>
2023-05-09 20:31:39 +0200

    Add dependencies

---
+++ SpeakFreely/src/main/java/com/speakfreely/speakfreely/SpeakFreelyApplication.java
@@ -0,0 +1,13 @@
+package com.speakfreely.speakfreely;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeakFreelyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpeakFreelyApplication.class, args);
+    }
+
+}
---
+++ SpeakFreely/src/test/java/com/speakfreely/speakfreely/SpeakFreelyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.speakfreely.speakfreely;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpeakFreelyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

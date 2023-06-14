97d815b3d8eb4fc4272040ff7965b2a47cf225ff
PaulinaW <76110024+paulina-iit@users.noreply.github.com>
2023-06-14 23:16:52 +0200

    ChatMessage draft + pom edits

---
+++ SpeakFreely/src/main/java/com/speakfreely/speakfreely/chat/ChatMessage.java
@@ -0,0 +1,45 @@
+package com.speakfreely.speakfreely.chat;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+// reprezentuje wiadomość czatu
+@Entity
+@Table(name = "chat_messages")
+public class ChatMessage {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String content;
+
+    @Column(nullable = false)
+    private LocalDateTime timestamp;
+
+    // Getters and setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
+}

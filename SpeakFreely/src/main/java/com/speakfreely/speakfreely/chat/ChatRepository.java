cd22864d55e780e6ab6ef32503705c8bc4d5f1f1
PaulinaW <76110024+paulina-iit@users.noreply.github.com>
2023-06-14 23:20:21 +0200

    ChatRepositoryDraft

---
+++ SpeakFreely/src/main/java/com/speakfreely/speakfreely/chat/ChatRepository.java
@@ -0,0 +1,15 @@
+package com.speakfreely.speakfreely.chat;
+
+import com.speakfreely.speakfreely.chat.ChatMessage;
+import com.speakfreely.speakfreely.model.Course;
+import com.speakfreely.speakfreely.model.Tutor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ChatRepository extends JpaRepository<ChatMessage, Long> {
+    List<ChatMessage> findByCourse(Course course);
+    List<ChatMessage> findByTutor(Tutor tutor);
+}
